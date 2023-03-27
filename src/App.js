343e3514475a0df09a70c8ddc6b4c9b954491a0f
mohamed271220 <mohamedaprahym@gmail.com>
2023-03-28 01:11:51 +0200

    monthly chart added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Customers from 'scenes/customers'
 import Transactions from 'scenes/Transactions'
 import Geography from 'scenes/geography'
 import Overview from 'scenes/overview'
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
 
 function App() {
 const mode = useSelector((state)=> state.global.mode)
@@ -29,6 +31,9 @@ const theme = useMemo(()=> createTheme(themeSettings(mode)),[mode]);
             <Route path="/transactions" element={<Transactions/>}/>
             <Route path="/geography" element={<Geography/>}/>
             <Route path="/overview" element={<Overview />} />
+            <Route path="/daily" element={<Daily />} />
+            <Route path="/monthly" element={<Monthly />} />
+
           </Route>
         </Routes>
       </ThemeProvider>
